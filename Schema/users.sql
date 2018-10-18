4b5770c798f2b71372202df267b19e2b40c4bfae
RyanMStewart <ryan86stewart@gmail.com>
2018-10-18 16:47:49 -0500

    Added users table

---
+++ Schema/users.sql
@@ -0,0 +1,4 @@
+insert into users (username, email, password) values ('mkenealy0', 'ddaubeny0@wufoo.com', 'A5cY6fws');
+insert into users (username, email, password) values ('kgascar1', 'nbere1@state.tx.us', 'jvuqAV0ujt');
+insert into users (username, email, password) values ('glockless2', 'dpeploe2@ow.ly', 'vPhaerFHqq');
+insert into users (username, email, password) values ('chabeshaw3', 'aloghan3@studiopress.com', 'NdANzGWb3wDu');
