a9f7df973e43cf21eaea0dc514c52fa9ae2c2a6a
Justin Luoma <justin@justinluoma.com>
2018-10-19 16:41:06 -0500

    Updated User model

--- src/main/java/com/codeup/studentdashboard/models/User.java
+++ src/main/java/com/codeup/studentdashboard/models/User.java
@@ -15,4 +15,52 @@ public class User {
 
     @Column(nullable = false, length = 98)
     private String password;
+
+    public User() {
+    }
+
+    public User(User copy){
+        id = copy.id;
+        username = copy.username;
+        email = copy.email;
+        password = copy.password;
+    }
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public long id() {
+        return id;
+    }
+
+    public void id(long id) {
+        this.id = id;
+    }
+
+    public String username() {
+        return username;
+    }
+
+    public void username(String username) {
+        this.username = username;
+    }
+
+    public String email() {
+        return email;
+    }
+
+    public void email(String email) {
+        this.email = email;
+    }
+
+    public String password() {
+        return password;
+    }
+
+    public void password(String password) {
+        this.password = password;
+    }
 }
