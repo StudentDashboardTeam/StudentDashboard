3dd69e9466f32f90ef0d9cd634c798cd5953269a
RyanMStewart <ryan86stewart@gmail.com>
2018-10-20 15:02:24 -0500

    Working Login

--- src/main/java/com/codeup/studentdashboard/controllers/AuthenticationController.java
+++ src/main/java/com/codeup/studentdashboard/controllers/AuthenticationController.java
@@ -2,10 +2,11 @@ package com.codeup.studentdashboard.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AuthenticationController {
-    @GetMapping("/login")
+    @PostMapping("/login")
     public String showLoginForm() {
         return "loginSuccess";
     }
