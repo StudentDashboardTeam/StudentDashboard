607a199e6d48d5bbc50bfd49cc692cd862427f0c
Justin Luoma <justin@justinluoma.com>
2018-10-23 12:02:07 -0500

    Updated event_type data with real event types and added seeder data

---
+++ src/main/java/com/codeup/studentdashboard/dao/repository/EventTypeRepository.java
@@ -0,0 +1,8 @@
+package com.codeup.studentdashboard.dao.repository;
+
+import com.codeup.studentdashboard.models.EventType;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EventTypeRepository extends CrudRepository<EventType, Long> {
+
+}
--- src/main/java/com/codeup/studentdashboard/seeder/DatabaseSeeder.java
+++ src/main/java/com/codeup/studentdashboard/seeder/DatabaseSeeder.java
@@ -1,7 +1,9 @@
 package com.codeup.studentdashboard.seeder;
 
+import com.codeup.studentdashboard.dao.repository.EventTypeRepository;
 import com.codeup.studentdashboard.dao.repository.HAUOptionsRepository;
 import com.codeup.studentdashboard.dao.repository.PaymentOptionsRepository;
+import com.codeup.studentdashboard.models.EventType;
 import com.codeup.studentdashboard.models.student.HAUOption;
 import com.codeup.studentdashboard.models.student.PaymentOption;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,18 +18,22 @@ import java.util.stream.StreamSupport;
 public class DatabaseSeeder {
     private PaymentOptionsRepository paymentOptionsRepository;
     private HAUOptionsRepository hauOptionsRepository;
+    private EventTypeRepository eventTypeRepository;
 
     @Autowired
     public DatabaseSeeder(PaymentOptionsRepository paymentOptionsRepository,
-                          HAUOptionsRepository hauOptionsRepository) {
+                          HAUOptionsRepository hauOptionsRepository,
+                          EventTypeRepository eventTypeRepository) {
         this.paymentOptionsRepository = paymentOptionsRepository;
         this.hauOptionsRepository = hauOptionsRepository;
+        this.eventTypeRepository = eventTypeRepository;
     }
 
     @EventListener
     public void seed(ContextRefreshedEvent event) {
         seedPaymentOptions();
         seedHAUOptions();
+        seedEventTypes();
     }
 
     private void seedPaymentOptions() {
@@ -74,6 +80,45 @@ public class DatabaseSeeder {
                     new HAUOption("Other")
             };
             hauOptionsRepository.save(Arrays.asList(hauOption));
+        }
+    }
+
+    private void seedEventTypes() {
+        long count = StreamSupport.stream(
+            eventTypeRepository.findAll().spliterator(),
+                false)
+                .count();
+        if (count < 27) {
+            EventType eventType[] = {
+                    new EventType("Applied"),
+                    new EventType("Phone Interview Scheduled"),
+                    new EventType("Phone Interview"),
+                    new EventType("Tour Scheduled"),
+                    new EventType("Tour"),
+                    new EventType("Problem Solving Challenge"),
+                    new EventType("Command Line Review Assigned"),
+                    new EventType("Command Line Review Completed"),
+                    new EventType("Behavior Interview Scheduled"),
+                    new EventType("Behavior Interview"),
+                    new EventType("Pre-Work Assigned"),
+                    new EventType("Pre-Work Assessment"),
+                    new EventType("Rejected"),
+                    new EventType("Acceptance Phone Call"),
+                    new EventType("Verbal Accepted"),
+                    new EventType("Verbal Rejected"),
+                    new EventType("Do Not Contact"),
+                    new EventType("Enrollment Email Send"),
+                    new EventType("Deposit Payed"),
+                    new EventType("Deposit Not Payed"),
+                    new EventType("Financial Call"),
+                    new EventType("Finance Arranged"),
+                    new EventType("Finance Not Arranged"),
+                    new EventType("Student"),
+                    new EventType("No show"),
+                    new EventType("Withdrawn"),
+                    new EventType("Graduate")
+            };
+            eventTypeRepository.save(Arrays.asList(eventType));
         }
     }
 }
