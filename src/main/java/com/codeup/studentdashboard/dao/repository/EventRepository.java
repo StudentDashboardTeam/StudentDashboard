2db09bd92d531eab6e6b8d68eede7f7206c922b2
Justin Luoma <justin@justinluoma.com>
2018-10-25 18:22:56 -0500

    Students page partially updated. Last event implemented. Maven dependencies for autoloading added

--- src/main/java/com/codeup/studentdashboard/dao/repository/EventRepository.java
+++ src/main/java/com/codeup/studentdashboard/dao/repository/EventRepository.java
@@ -1,8 +1,12 @@
 package com.codeup.studentdashboard.dao.repository;
 
 import com.codeup.studentdashboard.models.Events;
+import com.codeup.studentdashboard.models.Student;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface EventRepository extends CrudRepository<Events, Long> {
 
+    Events findFirstByStudentOrderByDateDesc(Student student);
 }
